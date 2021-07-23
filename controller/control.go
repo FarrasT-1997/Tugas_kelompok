040f6d2ba26f5db1e5349263302f26f4a5b5395c
AnggunPermata <anggunpermata.g789@gmail.com>
2021-07-23 11:24:51 +0700

    adding controller

--- controller/control.go
+++ controller/control.go
@@ -1,1 +1,87 @@
 package controller
+
+import (
+	"net/http"
+	"rest_api/database"
+	"strconv"
+
+	"github.com/labstack/echo"
+)
+
+func GetUsersController(c echo.Context) error {
+	users, err := database.GetUsers()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"users":  users,
+	})
+}
+
+func CreateUserController(c echo.Context) error {
+	user, err := database.CreateUser(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"users":  user,
+	})
+}
+
+func DeleteUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid id",
+		})
+	}
+	user, err := database.DeleteUser(id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":    user,
+	})
+}
+
+func GetUserController(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid id",
+		})
+	}
+	user, err := database.GetUser(id)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"users":  user,
+	})
+}
+
+func UpdateUserController(c echo.Context) error {
+	var user models.User
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "invalid id",
+		})
+	}
+	c.Bind(&user)
+	update_user, err := database.UpdateUser(id, user)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success",
+		"data":    update_user,
+	})
+}
