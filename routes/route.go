ae24c3cf40938d0267550766e1e3b2f94286723c
Farras Timorremboko <Farras>
2021-07-23 11:16:55 +0700

    change main and routes

--- routes/route.go
+++ routes/route.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"rest_api/controller"
+
+	"github.com/labstack/echo"
+)
+
+func New(e *echo.Echo) {
+	e.GET("/users/", controller.GetUserControllers)
+	e.GET("/users/:id", controller.GetOneUserControllers)
+	e.POST("/users/", controller.CreateUserControllers)
+	e.PUT("/users/:id", controller.EditUserControllers)
+	e.DELETE("/users/:id", controller.DeleteUserControllers)
+}
