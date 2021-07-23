bb390ec05f5c9d1cbb771c3ffe178ac1f2c0db13
AnggunPermata <anggunpermata.g789@gmail.com>
2021-07-23 12:06:24 +0700

    adding import

--- config/configuration.go
+++ config/configuration.go
@@ -1,11 +1,31 @@
 package config
 
 import (
+	"fmt"
+	"rest_api/models"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Init_DB() {
+
+	config := map[string]string{
+		"HTTP_USERNAME": "root",
+		"HTTP_PASSWORD": "aPasswordNeeded",
+		"HTTP_PORT":     "3306",
+		"HTTP_HOST":     "127.0.0.1",
+		"HTTP_NAME":     "new_schema",
+	}
+
+	ConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config["HTTP_USERNAME"],
+		config["HTTP_PASSWORD"],
+		config["HTTP_HOST"],
+		config["HTTP_PORT"],
+		config["HTTP_NAME"])
 	var e error
 	DB, e = gorm.Open(mysql.Open(ConnectionString), &gorm.Config{})
 	if e != nil {
