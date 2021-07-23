edb4305d262027a1630db88a6505fd70a1081bb8
Farras Timorremboko <Farras>
2021-07-23 11:53:26 +0700

    change route name fromn GetUserController -> GetUsersController & EditUserController -> UpdateUserController

--- routes/route.go
+++ routes/route.go
@@ -7,9 +7,9 @@ import (
 )
 
 func New(e *echo.Echo) {
-	e.GET("/users/", controller.GetUserController)
+	e.GET("/users/", controller.GetUsersController)
 	e.GET("/users/:id", controller.GetOneUserController)
 	e.POST("/users/", controller.CreateUserController)
-	e.PUT("/users/:id", controller.EditUserController)
+	e.PUT("/users/:id", controller.UpdateUserController)
 	e.DELETE("/users/:id", controller.DeleteUserController)
 }
